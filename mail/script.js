c6876cabd4e2547f88f1c28491b2c73e9ed6c1a2
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-10 15:19:37 +0200

    correzioni js

--- mail/script.js
+++ mail/script.js
@@ -3,14 +3,19 @@ console.log("Email utente: " + emailUtente);
 
 var emailList = ["ciccio.8@libero.it","antonio.pane@gmail.com","asky_08@hotmail.it"];
 
-for(var i = 0; i <= emailList.length; i++){
-    
-    if(emailList == emailUtente){
-    console.log("Email valida") 
-    }
- 
-    else{
-     console.log("Email non valida")
-    }
+var emailValida = [];
+
+for(var i = 0; i < emailList.length; i++){
+    var email = emailList[i];
 }
+
+if(email === emailUtente){
+    emailValida = i
+    console.log("Email Valida")
+}
+
+else{
+    console.log("Email non valida")
+}
+
 
