7bbe5325c5b91811f881f30b4a1c402190980783
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-11 18:14:16 +0200

    add controllo numeri

--- dadi/script.js
+++ dadi/script.js
@@ -1,11 +1,14 @@
-var numeroUtente = prompt("Scrivi un numero da 1 a 6")
+var numeroUtente = parseInt (prompt("Inserisci un numero da 1 a 6"))
 console.log(numeroUtente);
 
-var numeroComputer = Math.round(Math.random()*6); 
+var numeroComputer = Math.round(Math.random()*5+1); 
 console.log(numeroComputer)
 
 if(Number.isNaN (numeroUtente)){
-    alert("Inserire cifre numeriche") 
+    alert("Inserire cifre numeriche")
+    document.getElementById("numero-utente").innerHTML = "Partita Nulla" 
+
+    document.getElementById("numero-computer").innerHTML = "Partita Nulla"
 }
 
 if(numeroUtente > 6){
@@ -19,20 +22,33 @@ if(numeroUtente < 1){
 if(numeroUtente > numeroComputer){
     console.log ("Giocatore umano vince")
     document.getElementById("messaggio").innerHTML = "Giocatore umano vince"
+
+    document.getElementById("numero-utente").innerHTML = "Numero Utente = " + 
+    ('<em>' + numeroUtente + '</em>')
+
+    document.getElementById("numero-computer").innerHTML = "Numero Computer = " + 
+    ('<em>' + numeroComputer + '</em>')
 }
 
 else if (numeroComputer > numeroUtente){
     console.log ("Intelligenza artificiale conquista il mondo")
     document.getElementById("messaggio").innerHTML = "Intelligenza artificiale conquista il mondo"
+
+    document.getElementById("numero-utente").innerHTML = "Numero Utente = " + 
+    ('<em>' + numeroUtente + '</em>')
+
+    document.getElementById("numero-computer").innerHTML = "Numero Computer = " + 
+    ('<em>' + numeroComputer + '</em>')
 }
 
-else{
+else if(numeroUtente = numeroComputer){
     console.log ("Risultato Pari")
     document.getElementById("messaggio").innerHTML = "Risultato Pari"
 }
 
-document.getElementById("numero-utente").innerHTML = "Numero Utente = " + 
- ('<em>' + numeroUtente + '</em>')
+else{
+    console.log ("Partita Nulla")
+    document.getElementById("messaggio").innerHTML = "Partita Nulla"
+}
 
- document.getElementById("numero-computer").innerHTML = "Numero Computer = " + 
- ('<em>' + numeroComputer + '</em>')
+
