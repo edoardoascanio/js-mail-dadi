9d89571b4ee805896f968b0375028d7848f5a01c
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-11 17:18:34 +0200

    add if else

--- dadi/script.js
+++ dadi/script.js
@@ -4,10 +4,7 @@ console.log(numeroUtente);
 var numeroComputer = Math.round(Math.random()*6); 
 console.log(numeroComputer)
 
-if(Number.isNaN (numeroUtente)){
-    alert("Inserire cifre numeriche")
-    document.getElementById("numero-utente").innerHTML = "Inserire cifre numeriche" 
-}
+
 
 if(numeroUtente > 6){
     alert(" E' necessario scrivere un numero da 1 a 6")
@@ -22,9 +19,18 @@ if(numeroUtente > numeroComputer){
     document.getElementById("messaggio").innerHTML = "Giocatore umano vince"
 }
 
-else{
+else if (numeroComputer > numeroUtente){
     console.log ("Intelligenza artificiale conquista il mondo")
     document.getElementById("messaggio").innerHTML = "Intelligenza artificiale conquista il mondo"
+}
+
+else{
+    console.log ("Risultato Pari")
+    document.getElementById("messaggio").innerHTML = "Risultato Pari"
+}
+
+if(Number.isNaN (numeroUtente)){
+    alert("Inserire cifre numeriche") 
 }
 
 document.getElementById("numero-utente").innerHTML = "Numero Utente = " + 
